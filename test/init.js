7da7421b42cb398712d4207dfc14b44710e9cedf
akshat k <akshatkot@gmail.com>
2024-09-29 05:26:44 +0530

    testing file

--- test/init.js
+++ test/init.js
@@ -1,20 +1,10 @@
 const   Server=  require('../server/index.js')
-
 const  server  =  new Server()
 
-server.get("/",(req,res)=>{
+server.post("/",(req,res)=>{
 	console.log(req)
-	if(res){
-		res.write("HTTP/1.1 200 OK\n\n Hello World")
-		res.end()
-	}
-	else{
-		console.log("Aaise banyega coder")
-	
-	}
+	res.send({status:200})
 })
-
-
 server.listen(8080,()=>{
 	console.log("test")
 })
