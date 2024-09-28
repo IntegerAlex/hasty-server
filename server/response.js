97c6a1a65c9a910370004e1d611b243552929216
akshat k <akshatkot@gmail.com>
2024-09-29 05:25:55 +0530

    Response object file

---
+++ server/response.js
@@ -0,0 +1,41 @@
+class Response {
+    constructor(socket) {
+        this.socket = socket;
+    }
+
+    // Method to set status code
+    sendStatus(code) {
+        this.statusCode = code;
+        return this;
+    }
+
+    // Method to set headers
+    setHeader(key, value) {
+        this.headers[key] = value;
+        return this;
+    }
+
+    // Method to send a response with a body
+    send(body) {
+        const response = `HTTP/1.1 200 OK\n\n${body}`;
+        this.socket.write(response); // Send the complete response
+        this.socket.end(); // End the connection
+    }
+
+    // Method to send only the status
+    sendStatus(statusCode) {
+	const response = `HTTP/1.1 ${statusCode} \n\n`;
+	this.socket.write(response); // Send the complete response
+        this.socket.end(); // End the connection
+    }
+
+    // Helper method to format headers
+    formatHeaders() {
+        return Object.keys(this.headers)
+            .map(key => `${key}: ${this.headers[key]}`)
+            .join('\r\n');
+    }
+}
+
+module.exports = Response;
+
