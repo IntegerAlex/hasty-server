11fb521ceb56a78381ce6523e563f9dbb5331937
Prince Kumar Prasad <prince08833@gmail.com>
2024-10-01 07:15:59 +0530

    changed the bg-color of navigation and added a website logo in a navigation

--- doc/src/components/header.jsx
+++ doc/src/components/header.jsx
@@ -2,16 +2,20 @@ import Typography from "./general/typography";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center md:px-20">
+    <header className="bg-black text-white p-4 flex justify-between items-center md:px-20">
       <div className="flex items-center">
-        <Typography variant="h6">Hasty Server</Typography>
+        <img
+          src="../../public/logo.png"
+          alt="Hasty Server Logo"
+          className="w-20"
+        />
       </div>
       <nav>
         <ul className="flex space-x-4">
           <li>
             <a
               href="https://www.npmjs.com/package/hasty-server"
-              className="text-blue-300 hover:underline"
+              className="text-[#f3ce1f] hover:text-[#a89223]"
               target="_blank"
               rel="noopener noreferrer"
             >
--- doc/src/layout.jsx
+++ doc/src/layout.jsx
@@ -10,25 +10,25 @@ const Layout = ({ children }) => {
     <div className="flex flex-col min-h-screen w-full">
       <Header />
       <div className="flex flex-1">
-        <nav className={`w-64 bg-gray-100 p-4 ${isNavOpen ? "block" : "hidden"} md:block`}>
+        <nav className={`w-48 bg-gray-100 p-4 ${isNavOpen ? "block" : "hidden"} md:block`}>
           <ul className="space-y-4">
             <li>
-              <Link to="/" className="text-blue-500 hover:underline">
+              <Link to="/" className="text-black hover:text-gray-800">
                 <Typography variant="body1">Introduction</Typography>
               </Link>
             </li>
             <li>
-              <Link to="/features" className="text-blue-500 hover:underline">
+              <Link to="/features" className="text-black hover:text-gray-800">
                 <Typography variant="body1">Features</Typography>
               </Link>
             </li>
             <li>
-              <Link to="/installation" className="text-blue-500 hover:underline">
+              <Link to="/installation" className="text-black hover:text-gray-800">
                 <Typography variant="body1">Installation</Typography>
               </Link>
             </li>
             <li>
-              <Link to="/quickstart" className="text-blue-500 hover:underline">
+              <Link to="/quickstart" className="text-black hover:text-gray-800">
                 <Typography variant="body1">Quick Start</Typography>
               </Link>
             </li>
