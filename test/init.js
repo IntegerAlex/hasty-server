6e9303f72049cf8bb2fe72c380e7a906b64a8d48
Mahabub <mahabubx7@gmail.com>
2024-10-01 22:53:04 +0600

    test n http-status works now

--- test/init.js
+++ test/init.js
@@ -1,10 +1,31 @@
-const   Server=  require('../server/index.js')
+const  Server=  require('../server/index.js')
 const  server  =  new Server()
 
 server.get("/",(req,res)=>{
 	console.log(req)
-	res.json({status:200})
+	res.status(200).json({status:200})
 })
+
+server.post("/",(req,res)=>{
+	console.log(req)
+	res.status(201).json({status:201})
+})
+
+server.put("/",(req,res)=>{
+	console.log(req)
+	res.status(202).json({status:202})
+})
+
+server.patch("/",(req,res)=>{
+	console.log(req)
+	res.status(200).json({status:200})
+})
+
+server.delete("/",(req,res)=>{
+	console.log(req)
+	res.status(204).json({status:204})
+})
+
 server.listen(8080,()=>{
 	console.log("test")
 })
