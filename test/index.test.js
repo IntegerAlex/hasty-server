0211dbe56a90cb99511037ca4b1fa9ccc1b2ba99
akshat k <akshatkot@gmail.com>
2024-10-09 03:26:49 +0530

    Adding unit testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,10 @@
 const net = require('net');
-const Hasty = require('../server/index'); // Adjust the path as needed
-
+const Hasty = require('../server/index'); // Adjust the path as neededc
+const fs = require('fs');
 describe('Hasty Server', () => {
 
     let server;
 
-    afterEach((done) => {
-        if (server) {
-            server.close(() => {
-                done();
-            });
-        } else {
-            done();
-        }
-    });
 
     test('should call the correct callback for a GET request', done => {
         server = new Hasty();
@@ -38,6 +29,7 @@ describe('Hasty Server', () => {
                 // Assert that the response is what we expect
                 expect(data.toString()).toContain('GET request received');
                 client.end();
+		    server.close();
                 done();
             });
 
@@ -46,7 +38,7 @@ describe('Hasty Server', () => {
                 done(err);
             });
         });
-    }, 10000); // Increase the timeout to 10 seconds
+    }, 5000); // Increase the timeout to 10 seconds
 
     test('/GET json', done => {
         server = new Hasty();
@@ -55,31 +47,55 @@ describe('Hasty Server', () => {
         };
         server.get('/json', callback);
         server.listen(3000, () => {
-            console.log('Server is listening on port 3000');
-            const client = net.connect({ port: 3000 }, () => {
-                client.write('GET /json HTTP/1.1\r\nHost: localhost\r\n\r\n');
-            });
-		 client.on('data', (data) => {
-            console.log('Received data from server:', data.toString());
-            try {
-                // Check if the response is valid JSON
-                const jsonResponse =	data.toString().split('\r\n\r\n')[1];
-                console.log('jsonResponse:', jsonResponse);
-		JSON.parse(jsonResponse);
-                expect(true).toBe(true); // If JSON.parse doesn't throw an error, the test passes
-            } catch (e) {
-                // If parsing fails, the test should fail
-                expect('Invalid JSON').toBe('Valid JSON');
-            }
-            client.end();
-            done();
+		fetch('http://localhost:3000/json')
+		.then(response => {
+			return response.json();
+		}).then(data => {
+			expect(data).toEqual({ message: 'GET request received' });
+			server.close();
+			done();
+		}).catch(err => {
+			console.log(err);
+		});
         });
-            client.on('error', (err) => {
-                console.error('Client error:', err);
-                done(err);
+    }, 5000); // Increase the timeout to 10 seconds
+
+
+test('/GET sendFile', done => {
+    const server = new Hasty();
+    const callback = (req, res) => {
+        const path = require('path');
+        res.sendFile(path.join(__dirname, 'index.html'));
+    };
+
+    server.get('/file', callback);
+    server.listen(3000, () => {
+	    const path = require('path');
+        fetch('http://localhost:3000/file')
+            .then(response => {
+                // Check if the Content-Type header is as expected
+                expect(response.headers.get('Content-Type')).toBe('text/html');
+                return response.text(); // Read the response body
+            })
+            .then(data => {
+		const  stream =  fs.createReadStream(path.join(__dirname,'index.html'));
+		    stream.on('data', chunk => {	
+			expect(data).toEqual(chunk.toString()); // Check the response body
+			server.close();
+			    done();
+		    });
+		    stream.on('error', err => {
+			console.error(err);
+			done(err);
+		    })
+            })
+            .catch(err => {
+                console.log(err);
+                done(err); // Pass the error to the done callback
             });
-        });
-    }, 10000); // Increase the timeout to 10 seconds
+    });
+},10000);
+	
 
 });
 
