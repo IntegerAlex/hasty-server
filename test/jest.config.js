5e0b1e9ad1c9931be136b14fb3f89d75098ff6fe
JP-sDEV <jon.pablo80@gmail.com>
2024-10-12 10:40:59 -0400

    jest config

---
+++ test/jest.config.js
@@ -0,0 +1,15 @@
+// Log a message to remind developers how to see more detail from log messages
+console.log(
+  `Using LOG_LEVEL=${process.env.LOG_LEVEL}. Use 'debug' in env.jest for more detail`
+);
+
+// Set our Jest options, see https://jestjs.io/docs/configuration
+module.exports = {
+  verbose: true,
+  testTimeout: 5000,
+  coveragePathIgnorePatterns: [
+    "/node_modules/*",
+    "/test/httpParser.test.js", // ignore current tests
+    "/test/index.html",
+  ],
+};
