2b88e312cd747507e22078429a08e9f3b41c5076
akshat k <akshatkot@gmail.com>
2024-10-22 03:04:25 +0530

    Fixed edit this page links

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
+            'https://github.com/IntegerAlex/hasty-server.git',
         },
         blog: {
           showReadingTime: true,
@@ -55,7 +55,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/IntegerAlex/hasty-server.git',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
